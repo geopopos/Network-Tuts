64e14629128d42898d6e817b07296dc755f67581
Georgios Roros <groros2@icloud.com>
2022-11-21 15:39:10 -0700

    set up player waiting room Part 2 of Rayuuse' tutorial series https://www.youtube.com/watch?v=NOnk8mAwuJ8&list=PLRe0l8OGr7rcFTsWm3xyfCOP4NpH72vB1&index=2

--- GameClient/lobby/lobby.gd
+++ GameClient/lobby/lobby.gd
@@ -1,5 +1,25 @@
 extends Control
 
+onready var player_name = $CenterContainer/VBoxContainer/GridContainer/NameTextBox
+onready var selected_IP = $CenterContainer/VBoxContainer/GridContainer/IPTextBox
+onready var selected_port = $CenterContainer/VBoxContainer/GridContainer/PortTextBox
+onready var waiting_room = $WaitingRoom
+
+func _ready():
+	player_name.text = Save.save_data["Player_name"]
+	selected_IP.text = Server.DEFAULT_IP
+	selected_port.text = str(Server.DEFAULT_PORT)
 
 func _on_Button_pressed():
+	Server.selected_IP = selected_IP.text
+	Server.selected_port = int(selected_port.text)
 	Server._connect_to_server()
+	show_waiting_room()
+
+
+func _on_NameTextBox_text_changed(new_text):
+	Save.save_data["Player_name"] = player_name.text
+	Save.save_game()
+
+func show_waiting_room():
+	waiting_room.popup_centered()
